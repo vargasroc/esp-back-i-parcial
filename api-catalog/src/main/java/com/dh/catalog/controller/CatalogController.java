18a56691fda569d77e17b6ffc8a7dc1e2fb14e7c
Jonathan Victorica <51026637+jonathanvictorica@users.noreply.github.com>
2022-11-10 19:17:29 -0300

    Update CatalogController.java

--- api-catalog/src/main/java/com/dh/catalog/controller/CatalogController.java
+++ api-catalog/src/main/java/com/dh/catalog/controller/CatalogController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/catalog")
+@RequestMapping("/api/v1/catalog")
 public class CatalogController {
 
 	private final MovieServiceClient movieServiceClient;
