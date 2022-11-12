10f078ac495b42e138217fb43a2f9d5a729d3527
vargasroc <vargasroc@gmail.com>
2022-11-12 20:14:33 -0300

    se arreglo error de tipeo

--- api-catalog/src/main/java/com/dh/catalog/client/FeignClient.java
+++
@@ -1,4 +0,0 @@
-package com.dh.catalog.client;
-
-public @interface FeignClient {
-}
--- api-catalog/src/main/java/com/dh/catalog/client/MovieServiceClient.java
+++ api-catalog/src/main/java/com/dh/catalog/client/MovieServiceClient.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name="api-movie")
 public interface MovieServiceClient {
 
 	@GetMapping("/api/v1/movies/{genre}")
